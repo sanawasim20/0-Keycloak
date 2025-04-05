6d3aa393017c28d4e425fe2c8911bf62cfe4bc90
sanawasim20 <115491608+sanawasim20@users.noreply.github.com>
2025-04-05 11:34:18 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App.jsx';
 //import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import 'react-toastify/ReactToastify.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="1015028759454-7742mgtciocpbagh1dhd8u6gn5i96c95.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
